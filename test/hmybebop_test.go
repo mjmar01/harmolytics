452c6a9b81f20f397e1030f268af0b33ca098bb8
Markus Maring <mjmaring12@gmail.com>
2022-04-21 17:07:30 +0200

    De-/Encode Benchmarks

--- test/hmybebop_test.go
+++ test/hmybebop_test.go
@@ -6,56 +6,48 @@ import (
 	"fmt"
 	"github.com/mjmar01/harmolytics/pkg/hmybebop"
 	"github.com/mjmar01/harmolytics/pkg/types"
-	"math/big"
 	"testing"
-	"time"
 )
 
 func TestTransactionBebop(t *testing.T) {
 	t.Parallel()
-	tx := types.Transaction{
-		TxHash:    "0x98e9e65c1920a49f68cc523c8a5f5103d922fb8d250a859671b5a959aba9e2b1",
-		EthTxHash: "0xaed17cb61f9c446112f6c50163c65d1c42d022183c6de9652495fee35a208a4f",
-		Sender:    types.NewAddress("0x492064d08a3426fc15b7009301eb56bb285b6d08"),
-		Receiver:  types.NewAddress("0xbda99c8695986b45a0dd3979cc6f3974d9753d30"),
-		BlockNum:  24191303,
-		Timestamp: 24191303,
-		Value:     new(big.Int).SetInt64(0),
-		Method:    types.Method{},
-		Input:     "0xa69df4b5",
-		Logs: []types.TransactionLog{
-			{
-				TxHash:   "0x98e9e65c1920a49f68cc523c8a5f5103d922fb8d250a859671b5a959aba9e2b1",
-				LogIndex: 0,
-				Address:  types.NewAddress("0xbda99c8695986b45a0dd3979cc6f3974d9753d30"),
-				Topics: []string{
-					"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-					"0x000000000000000000000000bda99c8695986b45a0dd3979cc6f3974d9753d30",
-					"0x000000000000000000000000492064d08a3426fc15b7009301eb56bb285b6d08",
-				},
-				Data: "0x000000000000000000000000000000000000000000000038f7ab9a0605354e75",
-			},
-		},
-		Status:    0,
-		GasAmount: 61818,
-		GasPrice:  new(big.Int).SetInt64(33000000000),
-		ShardID:   0,
-		ToShardID: 0,
-	}
-	t1 := time.Now()
 	data, _ := hmybebop.EncodeTransaction(tx)
-	fmt.Println(time.Since(t1))
-	t2 := time.Now()
-	_, _ = hmybebop.DecodeTransaction(data)
-	fmt.Println(time.Since(t2))
+	out, _ := hmybebop.DecodeTransaction(data)
+	fmt.Println(out.TxHash)
+}
 
-	buff := new(bytes.Buffer)
-	t1 = time.Now()
-	enc := gob.NewEncoder(buff)
-	_ = enc.Encode(tx)
-	fmt.Println(time.Since(t1))
-	t2 = time.Now()
-	dec := gob.NewDecoder(bytes.NewReader(buff.Bytes()))
-	_ = dec.Decode(&tx)
-	fmt.Println(time.Since(t2))
+func BenchmarkBebopEncode(b *testing.B) {
+	var data []byte
+	for i := 0; i < b.N; i++ {
+		data, _ = hmybebop.EncodeTransaction(tx)
+	}
+	dump = data
+}
+
+func BenchmarkGobEncode(b *testing.B) {
+	var data []byte
+	for i := 0; i < b.N; i++ {
+		buff := new(bytes.Buffer)
+		enc := gob.NewEncoder(buff)
+		enc.Encode(tx)
+		data = buff.Bytes()
+	}
+	dump = data
+}
+
+func BenchmarkBebopDecode(b *testing.B) {
+	var tmp types.Transaction
+	for i := 0; i < b.N; i++ {
+		tmp, _ = hmybebop.DecodeTransaction(txBebop)
+	}
+	dump = tmp
+}
+
+func BenchmarkGobDecode(b *testing.B) {
+	var tmp types.Transaction
+	for i := 0; i < b.N; i++ {
+		dec := gob.NewDecoder(bytes.NewReader(txGob))
+		dec.Decode(&tmp)
+	}
+	dump = tmp
 }
---
+++ test/main.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"bytes"
+	"encoding/gob"
+	"github.com/mjmar01/harmolytics/pkg/hmybebop"
+	"github.com/mjmar01/harmolytics/pkg/types"
+	"math/big"
+)
+
+const (
+	url = "wss://ws.s0.t.hmny.io"
+)
+
+var tx types.Transaction
+var txBebop, txGob []byte
+var dump interface{}
+
+func init() {
+	tx = types.Transaction{
+		TxHash:    "0x98e9e65c1920a49f68cc523c8a5f5103d922fb8d250a859671b5a959aba9e2b1",
+		EthTxHash: "0xaed17cb61f9c446112f6c50163c65d1c42d022183c6de9652495fee35a208a4f",
+		Sender:    types.NewAddress("0x492064d08a3426fc15b7009301eb56bb285b6d08"),
+		Receiver:  types.NewAddress("0xbda99c8695986b45a0dd3979cc6f3974d9753d30"),
+		BlockNum:  24191303,
+		Timestamp: 24191303,
+		Value:     new(big.Int).SetInt64(0),
+		Method:    types.Method{},
+		Input:     "0xa69df4b5",
+		Logs: []types.TransactionLog{
+			{
+				TxHash:   "0x98e9e65c1920a49f68cc523c8a5f5103d922fb8d250a859671b5a959aba9e2b1",
+				LogIndex: 0,
+				Address:  types.NewAddress("0xbda99c8695986b45a0dd3979cc6f3974d9753d30"),
+				Topics: []string{
+					"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+					"0x000000000000000000000000bda99c8695986b45a0dd3979cc6f3974d9753d30",
+					"0x000000000000000000000000492064d08a3426fc15b7009301eb56bb285b6d08",
+				},
+				Data: "0x000000000000000000000000000000000000000000000038f7ab9a0605354e75",
+			},
+		},
+		Status:    0,
+		GasAmount: 61818,
+		GasPrice:  new(big.Int).SetInt64(33000000000),
+		ShardID:   0,
+		ToShardID: 0,
+	}
+
+	buff := new(bytes.Buffer)
+	enc := gob.NewEncoder(buff)
+	enc.Encode(tx)
+	txGob = buff.Bytes()
+
+	txBebop, _ = hmybebop.EncodeTransaction(tx)
+}
