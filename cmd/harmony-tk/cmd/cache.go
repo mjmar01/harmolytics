9c350de468ce0fc61091f6a1bc3283f242742bb9
Markus Maring <mjmaring12@gmail.com>
2022-04-25 01:25:52 +0200

    Added centralCache to cli

--- cmd/harmony-tk/cmd/cache.go
+++ cmd/harmony-tk/cmd/cache.go
@@ -2,15 +2,30 @@ package cmd
 
 import (
 	"github.com/mjmar01/harmolytics/internal/helper"
+	"github.com/mjmar01/harmolytics/pkg/cache"
 	"github.com/spf13/cobra"
 )
 
 var cachePath string
 
+var centralCache *cache.Cache
+
 // cacheCmd represents the cache command
 var cacheCmd = &cobra.Command{
 	Use:   "cache",
 	Short: "Interact with and managed the local cache",
+}
+
+func openCache(cmd *cobra.Command, args []string) error {
+	c, err := cache.NewCache(&cache.Opts{
+		CacheDir:            cachePath,
+		PreLoadTransactions: false,
+	})
+	if err != nil {
+		return err
+	}
+	centralCache = c
+	return nil
 }
 
 func init() {
